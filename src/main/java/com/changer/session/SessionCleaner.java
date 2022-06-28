079ee1c5e5f28b724d84c05c18f503059191f08a
Danyl Miniailo <danyl.miniailo@care.com>
2022-06-28 17:14:12 +0300

    fix

--- src/main/java/com/changer/session/SessionCleaner.java
+++ src/main/java/com/changer/session/SessionCleaner.java
@@ -4,7 +4,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-
 class SessionCleaner {
     private final SessionStorage storage;
 
